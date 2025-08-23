05ee9497a188763bb4d75984ac4befa7eb63e8db
ajharul <mdajharulislam327@gmail.com>
2025-08-24 00:15:42 +0600

    add comments

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -23,7 +23,7 @@ export const register = async (req, res) => {
       });
     }
 
-    // Create new user
+    // Create new users
     const hashedPassword = await bcrypt.hash(password, 10);
     const user = await User.create({ name, email, password: hashedPassword });
 
